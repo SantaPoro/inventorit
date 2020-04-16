2212d7a21f91a7c6109ee0de885d0396adccf6db
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-04-16 21:15:01 +0200

    Added session and redis for session store

--- packages/backend/src/apollo.ts
+++ packages/backend/src/apollo.ts
@@ -1,4 +1,4 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 
 async function createApolloServer() {
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,28 +1,53 @@
 import 'reflect-metadata';
 
+import RedisSession from 'connect-redis';
 import dotenv from 'dotenv';
+import express from 'express';
+import session from 'express-session';
+import redis from 'redis';
 import { createConnection } from 'typeorm';
 
 import createApolloServer from './apollo';
 
 dotenv.config();
 
+const RedisSessionStore = RedisSession(session);
+
+const redisClient = redis.createClient({
+	host: 'redis',
+	password: String(process.env.REDIS_PASSWORD),
+});
+
 (async function () {
-	await createConnection({
-		type: 'postgres',
-		host: process.env.DB_HOST,
-		port: Number(process.env.DB_PORT),
-		database: process.env.DB_DATABASE,
-		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		entities: [__dirname + '/entities/*.{ts,js}'],
-		synchronize: process.env.NODE_ENV === 'development',
-		logging: process.env.NODE_ENV === 'development',
-	});
+	try {
+		await createConnection({
+			type: 'postgres',
+			host: process.env.DB_HOST,
+			port: Number(process.env.DB_PORT),
+			database: process.env.DB_DATABASE,
+			username: process.env.DB_USERNAME,
+			password: process.env.DB_PASSWORD,
+			entities: [__dirname + '/entities/*.{ts,js}'],
+			synchronize: process.env.NODE_ENV === 'development',
+			logging: process.env.NODE_ENV === 'development',
+		});
 
-	const server = await createApolloServer();
+		const app = express();
 
-	server.listen(3000).then(({ url }) => {
-		console.log(`Server ready at ${url}`);
-	});
+		app.use(
+			session({
+				store: new RedisSessionStore({ client: redisClient }),
+				secret: String(process.env.SESSION_SECRET),
+				resave: false,
+				saveUninitialized: false,
+			}),
+		);
+
+		const server = await createApolloServer();
+		server.applyMiddleware({ app });
+
+		app.listen(3000, () => console.log('Server ready at 3000'));
+	} catch (e) {
+		console.log(e);
+	}
 })();
