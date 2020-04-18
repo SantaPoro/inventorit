166b81091c04fd3108a42d57908a6902b7acf637
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-04-19 00:14:57 +0200

    Added apollo provider

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -3,13 +3,23 @@ import 'antd/dist/antd.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import AppScreen from './screens/app';
 
+const client = new ApolloClient({
+	cache: new InMemoryCache(),
+	link: new HttpLink({
+		uri: String(process.env.GRAPHQL_API_ENDPOINT),
+	}),
+});
+
 ReactDOM.render(
 	<Router>
-		<AppScreen />
+		<ApolloProvider client={client}>
+			<AppScreen />
+		</ApolloProvider>
 	</Router>,
 	document.getElementById('root'),
 );
