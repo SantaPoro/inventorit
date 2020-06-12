8b3a44fec55f17363e44caa37f013314e701814c
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-06-12 11:35:37 +0200

    added relay modern network layer

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -5,7 +5,7 @@ import { BrowserProtocol, queryMiddleware } from 'farce';
 import { createFarceRouter, createRender } from 'found';
 import { Resolver } from 'found-relay';
 
-import environment from './relay-environment';
+import relayEnvironment from './relay-environment';
 import { routeConfig } from './routes';
 
 const Router = createFarceRouter({
@@ -15,7 +15,7 @@ const Router = createFarceRouter({
 	render: createRender({}),
 });
 
-ReactDOM.render(<Router resolver={new Resolver(environment)} />, document.getElementById('root'));
+ReactDOM.render(<Router resolver={new Resolver(relayEnvironment)} />, document.getElementById('root'));
 
 if ((module as any).hot) {
 	(module as any).hot.accept();
--- packages/frontend/src/relay-environment.ts
+++ packages/frontend/src/relay-environment.ts
@@ -1,24 +1,59 @@
-import { Environment, Network, RecordSource, RequestParameters, Store, Variables } from 'relay-runtime';
+import {
+	cacheMiddleware,
+	errorMiddleware,
+	loggerMiddleware,
+	perfMiddleware,
+	progressMiddleware,
+	RelayNetworkLayer,
+	retryMiddleware,
+	uploadMiddleware,
+	urlMiddleware,
+} from 'react-relay-network-modern';
+import { Environment, RecordSource, Store } from 'relay-runtime';
 
-function fetchQuery(request: RequestParameters, variables: Variables) {
-	return fetch('/graphql', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
+const __DEV__ = process.env.NODE_ENV === 'development';
+
+const source = new RecordSource();
+const store = new Store(source);
+
+const network = new RelayNetworkLayer([
+	cacheMiddleware({
+		size: 100, // max 100 requests
+		ttl: 15 * 60 * 1000, // 15 minutes
+	}),
+	urlMiddleware({
+		url: () => Promise.resolve('/graphql'),
+	}),
+	__DEV__ ? loggerMiddleware() : null,
+	__DEV__ ? errorMiddleware() : null,
+	__DEV__ ? perfMiddleware() : null,
+	retryMiddleware({
+		fetchTimeout: 15 * 1000,
+		retryDelays: attempt => Math.pow(2, attempt + 4) * 100, // or simple array [3200, 6400, 12800, 25600, 51200, 102400, 204800, 409600],
+		beforeRetry: ({ abort, attempt }) => {
+			if (attempt > 10) {
+				abort();
+			}
 		},
-		body: JSON.stringify({
-			query: request.text,
-			variables,
-		}),
-		credentials: 'include',
-	}).then(response => {
-		return response.json();
-	});
-}
+		statusCodes: [500, 503, 504],
+	}),
+	progressMiddleware({
+		onProgress: (current, total) => {
+			console.log('Downloaded: ' + current + ' B, total: ' + total + ' B');
+		},
+	}),
+	uploadMiddleware(),
+	next => async req => {
+		req.fetchOpts.credentials = 'same-origin'; // allow to send cookies (sending credentials to same domains)
 
-const environment = new Environment({
-	network: Network.create(fetchQuery),
-	store: new Store(new RecordSource()),
-});
+		console.log('RelayRequest', req);
+		const res = await next(req);
+		console.log('RelayResponse', res);
 
-export default environment;
+		return res;
+	},
+]);
+
+const relayEnvironment = new Environment({ network, store });
+
+export default relayEnvironment;
